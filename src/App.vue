d364276a40cd60db9a129d97e4c88995605137ea
yenilikci <m.melihcelik017@outlook.com>
2021-08-29 17:30:54 +0300

    Create project, Add vue-router and Navbar component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app"><Navbar /></div>
+</template>
+
+<script>
+  import globalComponent from "./components/globalComponent";
+  export default {
+    components: {
+      ...globalComponent,
+    },
+  };
+</script>
+
+<style></style>
